f77137fc4edc1fb538be7a8c7648dec1fb1df9ad
trsreerenjini <trsreerenjini@gmail.com>
2026-04-23 15:47:11 +0530

    Add files via upload

---
+++ fi.java
@@ -0,0 +1,19 @@
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+public class fi
+{
+	public static void main(String[] args)throws IOException
+	{
+		FileInputStream source=new FileInputStream("source.txt");
+		FileOutputStream d=new FileOutputStream("des.txt");
+		int i;
+		while((i=source.read())!=-1)
+		{
+			d.write(i);
+		}
+		System.out.println("copied");
+		source.close();
+		d.close();
+	}
+}
